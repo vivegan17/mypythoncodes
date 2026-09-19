56fa3bcc938ca86b62bbd4f5d7a58341a99dbd97
VIVEGAN <walterwhite4750@gmail.com>
2026-09-19 13:50:28 +0530

    simple

---
+++ codersitecodes/numberoperations.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+sum = num1 + num2
+product = num1 * num2
+remainder = num1 % num2
+print("Sum:", sum)
+print("Product:", product)
+print("Remainder:", remainder)
---
+++ codersitecodes/timeconverter.py
@@ -0,0 +1,5 @@
+totalduration = int(input())
+hours = totalduration // 60
+minutes = totalduration % 60
+print("Hours:", hours)
+print("Minutes:", minutes)
